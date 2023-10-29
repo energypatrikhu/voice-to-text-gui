c612081cda54553e7742bb9a4de25f368f632f9a
EnergyPatrikHU <energyhun24@gmail.com>
2023-10-29 19:40:04 +0100

    small refactor

--- src/components/Settings/Section.svelte
+++ src/components/Settings/Section.svelte
@@ -2,9 +2,14 @@
 	export let useSplitterLine = true;
 </script>
 
-<div class="w-full {useSplitterLine ? 'border-b border-b-neutral-600' : ''} px-4 py-2 flex flex-col gap-2">
-	<div class="text-3xl">
-		<slot name="title" />
+<div class="w-full {useSplitterLine ? 'border-b border-b-neutral-600' : ''} px-4 py-2 flex flex-col gap-4">
+	<div>
+		<div class="text-3xl">
+			<slot name="title" />
+		</div>
+		<div class="pl-1 text-neutral-300">
+			<slot name="description" />
+		</div>
 	</div>
 	<slot name="content" />
 </div>
