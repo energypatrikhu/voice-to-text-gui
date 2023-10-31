475865f617cee41603b2773bba0939986feeb9c7
EnergyPatrikHU <energyhun24@gmail.com>
2023-10-31 22:02:55 +0100

    upgrade rm helper

--- rm.mjs
+++ rm.mjs
@@ -1,5 +1,33 @@
-import { rmSync } from 'fs';
+import { readdirSync, rmSync, statSync } from 'fs';
+import { join, resolve } from 'path';
 
-for (const path in process.argv.slice(1)) {
-	rmSync(path, { recursive: true, force: true });
+for (const rawPath in process.argv.slice(1)) {
+	const path = resolve(rawPath);
+	const stat = statSync(path);
+
+	if (stat.isFile()) {
+		try {
+			rmSync(path, { recursive: true, force: true });
+			console.log('Removed:', path);
+		} catch {
+			console.log('Failed to remove:', path);
+		}
+	} else if (stat.isDirectory()) {
+		(function __remover(absPath) {
+			for (const dirent of readdirSync(absPath, { withFileTypes: true })) {
+				const direntDir = join(absPath, dirent.name);
+
+				if (dirent.isFile()) {
+					try {
+						rmSync(direntDir, { recursive: true, force: true });
+						console.log('Removed:', direntDir);
+					} catch {
+						console.log('Failed to remove:', direntDir);
+					}
+				} else if (dirent.isDirectory()) {
+					__remover(direntDir);
+				}
+			}
+		})(path);
+	}
 }
