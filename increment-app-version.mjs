6046e4fa10dcd78fdecdcf8f3be6676ceaea988f
EnergyPatrikHU <energyhun24@gmail.com>
2023-10-31 18:12:13 +0100

    fix auto version incrementer

--- increment-app-version.mjs
+++ increment-app-version.mjs
@@ -2,8 +2,20 @@ import { readFile, writeFile } from 'fs/promises';
 
 const packageJson = JSON.parse(await readFile('./package.json', { encoding: 'utf-8' }));
 
-const currentVersion = parseInt(packageJson.version.replace(/\./g, ''));
+const versionNumbers = packageJson.version.split('.').map(function (stringNumber) {
+	return parseInt(stringNumber);
+});
 
-packageJson.version = (currentVersion + 1).toString().split('').join('.');
+versionNumbers[2] += 1;
+for (let i = versionNumbers.length - 1; i > 0; i--) {
+	console.log({ i }, versionNumbers[i]);
+
+	if (versionNumbers[i] > 9) {
+		versionNumbers[i] = 0;
+		versionNumbers[i - 1] += 1;
+	}
+}
+
+packageJson.version = versionNumbers.join('.');
 
 await writeFile('./package.json', JSON.stringify(packageJson, null, '\t'), { encoding: 'utf-8' });
