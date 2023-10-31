3ce818cda230c4a731723428f9ea76aac33d2a83
EnergyPatrikHU <energyhun24@gmail.com>
2023-10-31 22:43:31 +0100

    allow prerelease if beta

--- src/electron/electron.ts
+++ src/electron/electron.ts
@@ -90,6 +90,7 @@ async function createMainWindow() {
 		mainWindow = null;
 	});
 
+	autoUpdater.allowPrerelease = isBeta;
 	autoUpdater.checkForUpdatesAndNotify();
 
 	if (isDev) loadVite(port);
@@ -109,4 +110,26 @@ app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
+});
+
+autoUpdater.on('checking-for-update', () => {
+	console.log('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+	console.log('Update available.');
+});
+autoUpdater.on('update-not-available', (info) => {
+	console.log('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+	console.log('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+	let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+	log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+	log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+	console.log(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+	console.log('Update downloaded');
 });
