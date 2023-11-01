3bd7caba6bef35d522e909f8b2c26185dae2854c
EnergyPatrikHU <energyhun24@gmail.com>
2023-11-01 01:01:30 +0100

    set update checking to true

--- src/electron/libs/commands/update-app.ts
+++ src/electron/libs/commands/update-app.ts
@@ -5,6 +5,7 @@ import { cmd } from '../command-handler.js';
 
 cmd.registerCommand(
 	async function (speechSynthesis) {
+		__app.checkingForUpdate = true;
 		__app.console.log(__app.dictionary.textFeedback.commands.updateApp.checkingUpdate);
 
 		if (await autoUpdater.checkForUpdatesAndNotify()) {
