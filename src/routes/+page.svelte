dab66b9035d0c54cb29e7a2210f05f55550da1f8
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-18 19:50:47 +0100

    typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Header from '$components/Header.svelte';
 	import Layout from '$components/Layout.svelte';
 	import Console from '$components/Console.svelte';
-	import TextInput from '../components/TextInput.svelte';
+	import TextInput from '$components/TextInput.svelte';
 </script>
 
 <Header />
