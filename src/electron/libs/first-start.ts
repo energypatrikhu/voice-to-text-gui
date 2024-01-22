eaecedd0689acd008b0f0b20c1c8913b3b18c529
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-22 19:43:06 +0100

    display error msg if exists, remove after unzip

--- src/electron/libs/first-start.ts
+++ src/electron/libs/first-start.ts
@@ -1,25 +1,33 @@
 import AdmZip from 'adm-zip';
 import { existsSync } from 'fs';
+import { rm } from 'fs/promises';
 import { resolve } from 'path';
 
 import { __app } from './app.js';
 
-function uncompressChrome() {
-	const chromeZipPath = resolve('./resources/chrome.zip');
+async function uncompressChrome() {
+	try {
+		const chromeZipPath = resolve('./resources/chrome.zip');
 
-	if (!existsSync(chromeZipPath)) return;
+		if (!existsSync(chromeZipPath)) return;
 
-	const chromeZipOutputPath = resolve('./resources');
+		const chromeZipOutputPath = resolve('./resources');
 
-	const zip = new AdmZip(chromeZipPath);
+		const zip = new AdmZip(chromeZipPath);
 
-	zip.extractAllTo(chromeZipOutputPath);
+		zip.extractAllTo(chromeZipOutputPath);
+
+		await rm(chromeZipPath, { force: true, recursive: true });
+	} catch (error: any) {
+		__app.console.debugErrorLog(error.message ?? error);
+		throw error;
+	}
 }
 
 export async function firstStart() {
 	if (__app.isDev) return;
 
-	uncompressChrome();
+	await uncompressChrome();
 
 	__app.config.update.firstStart = false;
 }
