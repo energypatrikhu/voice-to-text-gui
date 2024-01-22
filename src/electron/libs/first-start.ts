0dab10fd1497b52fc8cadefab6ab75ae44549fd1
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-22 19:33:12 +0100

    switch '7z' with 'zip'

--- src/electron/libs/first-start.ts
+++ src/electron/libs/first-start.ts
@@ -1,25 +1,25 @@
+import AdmZip from 'adm-zip';
 import { existsSync } from 'fs';
 import { resolve } from 'path';
 
 import { __app } from './app.js';
 
-// import { fullArchive } from 'node-7z-archive';
-const { fullArchive } = require('node-7z-archive');
+function uncompressChrome() {
+	const chromeZipPath = resolve('./resources/chrome.zip');
 
-async function uncompress7zChrome() {
-	const chrome7zPath = resolve('./resources/chrome.7z');
+	if (!existsSync(chromeZipPath)) return;
 
-	if (!existsSync(chrome7zPath)) return;
+	const chromeZipOutputPath = resolve('./resources');
 
-	const chrome7zOutputPath = resolve('./resources');
+	const zip = new AdmZip(chromeZipPath);
 
-	await fullArchive(chrome7zPath, chrome7zOutputPath);
+	zip.extractAllTo(chromeZipOutputPath);
 }
 
 export async function firstStart() {
 	if (__app.isDev) return;
 
-	await uncompress7zChrome();
+	uncompressChrome();
 
 	__app.config.update.firstStart = false;
 }
