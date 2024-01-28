4b09a82ed29cf6ffd35ad9f41013541e3376c36f
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-28 06:03:33 +0100

    remove debugLog

--- src/electron/libs/7z.ts
+++ src/electron/libs/7z.ts
@@ -5,7 +5,5 @@ import { __app } from './app.js';
 
 export function extractArchive(filename: string, outputPath: string = '') {
 	const _7z = resolve(join(__app.resources, 'helpers', '7z.exe'));
-	const cmd = `"${_7z}" x -o"${join(__app.resources, outputPath)}" "${filename}"`;
-	__app.console.debugLog(cmd);
-	return execSync(cmd);
+	return execSync(`"${_7z}" x -o"${join(__app.resources, outputPath)}" "${filename}"`);
 }
