cc725a70ec163c1928a4763164449a01eb185abd
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-28 05:26:35 +0100

    use `spawnSync` instead of `spawn`

--- src/electron/libs/7z.ts
+++ src/electron/libs/7z.ts
@@ -1,13 +1,9 @@
-import { spawn } from 'child_process';
+import { spawnSync } from 'child_process';
 import { join, resolve } from 'path';
 
 import { __app } from './app.js';
 
 export function extractArchive(filename: string, outputPath: string = '') {
 	const _7z = resolve(join(__app.resources, 'helpers', '7z.exe'));
-	const _7zProcess = spawn(_7z, ['x', `-o${join(__app.resources, outputPath)}`, filename]);
-
-	return new Promise<void>((promiseResolve) => {
-		_7zProcess.on('exit', promiseResolve);
-	});
+	return spawnSync(_7z, ['x', `-o${join(__app.resources, outputPath)}`, filename]);
 }
