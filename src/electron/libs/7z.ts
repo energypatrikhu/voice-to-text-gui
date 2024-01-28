c7ec9920f603b8b535701dfc14edd7be620fd4e1
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-28 05:48:21 +0100

    potential fix to exec 7z

--- src/electron/libs/7z.ts
+++ src/electron/libs/7z.ts
@@ -5,7 +5,7 @@ import { __app } from './app.js';
 
 export function extractArchive(filename: string, outputPath: string = '') {
 	const _7z = resolve(join(__app.resources, 'helpers', '7z.exe'));
-	const cmd = `${_7z} x -o${join(__app.resources, outputPath)} ${filename}`;
+	const cmd = `"${_7z}" x -o"${join(__app.resources, outputPath)}" "${filename}"`;
 	__app.console.debugLog(cmd);
 	return execSync(cmd);
 }
