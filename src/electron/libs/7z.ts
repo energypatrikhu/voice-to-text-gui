3ce0b0652f66e4b95526b6a6bb044712cd382dfd
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-28 01:51:24 +0100

    add 7z binary & helper

---
+++ src/electron/libs/7z.ts
@@ -0,0 +1,14 @@
+import { spawn } from 'child_process';
+import { join, resolve } from 'path';
+
+import { __app } from './app.js';
+
+const _7z = resolve(join(__app.resources, 'helpers', '7z.exe'));
+
+export function extractArchive(filename: string, outputPath: string = '') {
+	const _7zProcess = spawn(_7z, ['x', `-o${join(__app.resources, outputPath)}`, filename]);
+
+	return new Promise<void>((promiseResolve) => {
+		_7zProcess.on('close', promiseResolve);
+	});
+}
