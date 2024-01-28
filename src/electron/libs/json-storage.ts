e08ec8ea6f9c9080bc883461dccab394b67eb591
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-28 01:52:27 +0100

    replace electron's app.getPath with __app.resources

--- src/electron/libs/json-storage.ts
+++ src/electron/libs/json-storage.ts
@@ -1,10 +1,11 @@
-import { app } from 'electron';
 import { existsSync } from 'fs';
 import { mkdir, readFile, writeFile } from 'fs/promises';
 import { join, resolve } from 'path';
 
+import { __app } from './app.js';
+
 export async function saveJson(name: string, data: any) {
-	const userDataFolder = process.env.NODE_ENV === 'dev' ? resolve('./local') : join(app.getPath('userData'), app.name);
+	const userDataFolder = process.env.NODE_ENV === 'dev' ? resolve('./local') : __app.userDataFolder;
 
 	if (!existsSync(userDataFolder)) {
 		await mkdir(userDataFolder, { recursive: true });
@@ -15,7 +16,7 @@ export async function saveJson(name: string, data: any) {
 
 export async function loadJson<T>(name: string): Promise<T | null> {
 	try {
-		const userDataFolder = process.env.NODE_ENV === 'dev' ? resolve('./local') : join(app.getPath('userData'), app.name);
+		const userDataFolder = process.env.NODE_ENV === 'dev' ? resolve('./local') : __app.userDataFolder;
 
 		if (!existsSync(join(userDataFolder, `${name}.json`))) {
 			return null;
