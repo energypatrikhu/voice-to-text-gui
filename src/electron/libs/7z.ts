bfa4e21c460e5dd8317b7016d563efe6dc4a8ed1
EnergyPatrikHU <energyhun24@gmail.com>
2024-01-28 05:41:29 +0100

    switch spawn to exec

--- src/electron/libs/7z.ts
+++ src/electron/libs/7z.ts
@@ -1,9 +1,11 @@
-import { spawnSync } from 'child_process';
+import { execSync } from 'child_process';
 import { join, resolve } from 'path';
 
 import { __app } from './app.js';
 
 export function extractArchive(filename: string, outputPath: string = '') {
 	const _7z = resolve(join(__app.resources, 'helpers', '7z.exe'));
-	return spawnSync(_7z, ['x', `-o${join(__app.resources, outputPath)}`, filename]);
+	const cmd = `${_7z} x -o${join(__app.resources, outputPath)} ${filename}`;
+	__app.console.debugLog(cmd);
+	return execSync(cmd);
 }
