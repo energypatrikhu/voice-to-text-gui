216f3829400cc0d59829dd58856a01ba8d8f3eba
EnergyPatrikHU <energyhun24@gmail.com>
2024-03-16 22:08:02 +0100

    Fix printText function in press-keys.ts

--- src/electron/libs/press-keys.ts
+++ src/electron/libs/press-keys.ts
@@ -10,16 +10,17 @@ export function printText(output: string, isCommand: boolean = false) {
 			}
 
 			const outputSegments = output.match(/\S.{1,90}(?=\s|$)/g);
-
 			const delay = __app.config.output.animated ? __app.config.output.typingDelay : 0;
 
 			for (const _output of outputSegments!) {
 				if (__app.config.others.mtaConsoleInputMode || isCommand) {
 					await keyboard.sendKey('f8', delay, delay);
-					await keyboard.printText(_output, delay, delay);
+				}
+
+				await keyboard.printText(_output, delay, delay);
+
+				if (__app.config.others.mtaConsoleInputMode || isCommand) {
 					await keyboard.sendKeys(['enter', 'f8'], delay, delay);
-				} else {
-					await keyboard.printText(_output, delay, delay);
 				}
 			}
 
