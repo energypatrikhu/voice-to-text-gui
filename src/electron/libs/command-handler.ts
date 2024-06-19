3eb30b91e93449cda5a52e0310702a2778bb84c1
EnergyPatrikHU <energypatrikhu@proton.me>
2024-06-19 20:42:40 +0200

    Fix macros

--- src/electron/libs/command-handler.ts
+++ src/electron/libs/command-handler.ts
@@ -15,141 +15,163 @@ type Command$Func = (speechSynthesis: SpeechSynthesisEngine, ...args: Array<any>
 type Macro$Handler = string;
 
 interface Command {
-	name: Command$Name;
-	availability: Command$Availability;
-	syntax: Command$Syntax;
-	handler: Command$Handler;
-	description: Command$Description;
-	func: Command$Func;
-	marco: boolean;
+  name: Command$Name;
+  availability: Command$Availability;
+  syntax: Command$Syntax;
+  handler: Command$Handler;
+  description: Command$Description;
+  func: Command$Func;
+  marco: boolean;
 }
 
 interface CmdParsed {
-	handler: Command$Handler;
-	args: any;
+  handler: Command$Handler;
+  args: any;
 }
 
 class CommandHandler {
-	private commands: Array<Command> = [];
-	private macros: Array<Macro> = __app.macros;
-	private speechSynthesis!: SpeechSynthesisEngine;
+  private commands: Array<Command> = [];
+  private macros: Array<Macro> = __app.macros;
+  private speechSynthesis!: SpeechSynthesisEngine;
 
-	get commandList() {
-		return this.commands;
-	}
-	get macroList() {
-		return this.macros;
-	}
+  get commandList() {
+    return this.commands;
+  }
+  get macroList() {
+    return this.macros;
+  }
 
-	async init(speechSynthesis: SpeechSynthesisEngine) {
-		this.speechSynthesis = speechSynthesis;
+  async init(speechSynthesis: SpeechSynthesisEngine) {
+    this.speechSynthesis = speechSynthesis;
 
-		await this.initCommands();
-	}
+    await this.initCommands();
+  }
 
-	private async initCommands() {
-		const commandFiles = ['./commands/command-list.js', './commands/active-window.js', './commands/exit.js', './commands/exit-mta.js', './commands/mta-mode.js', './commands/show-active-buttons.js', './commands/update-app.js'];
+  private async initCommands() {
+    const commandFiles = [
+      './commands/command-list.js',
+      './commands/active-window.js',
+      './commands/exit.js',
+      './commands/exit-mta.js',
+      './commands/mta-mode.js',
+      './commands/show-active-buttons.js',
+      './commands/update-app.js',
+    ];
 
-		for (const file of commandFiles) {
-			require(file);
-		}
-	}
+    for (const file of commandFiles) {
+      require(file);
+    }
+  }
 
-	registerCommand(func: Command$Func, [availability, name, syntax, handler, description]: [Command$Availability, Command$Name, Command$Syntax, Command$Handler, Command$Description]) {
-		this.commands.push({
-			availability,
-			name,
-			syntax: syntax !== null ? syntax : typeof handler === 'object' ? __app.config.commands.prefix + handler.join(` | ${__app.config.commands.prefix}`) : __app.config.commands.prefix + handler,
-			handler,
-			description,
-			func,
-			marco: false,
-		});
-	}
+  registerCommand(
+    func: Command$Func,
+    [availability, name, syntax, handler, description]: [
+      Command$Availability,
+      Command$Name,
+      Command$Syntax,
+      Command$Handler,
+      Command$Description,
+    ],
+  ) {
+    this.commands.push({
+      availability,
+      name,
+      syntax:
+        syntax !== null
+          ? syntax
+          : typeof handler === 'object'
+          ? __app.config.commands.prefix + handler.join(` | ${__app.config.commands.prefix}`)
+          : __app.config.commands.prefix + handler,
+      handler,
+      description,
+      func,
+      marco: false,
+    });
+  }
 
-	private async callMacro(handler: Macro$Handler, outputLocation: 'return' | 'write') {
-		const filteredMacros = this.macros.filter((macro) => macro.handler === handler);
+  private async callMacro(handler: Macro$Handler, outputLocation: 'return' | 'write') {
+    const macro = this.macros.find((macro) => macro.handler === handler);
 
-		if (filteredMacros.length > 0) {
-			const macro = filteredMacros[0];
+    if (macro) {
+      if (outputLocation === 'return') {
+        return macro.text;
+      }
 
-			if (outputLocation === 'return') {
-				return macro.text;
-			}
+      if (__app.config.others.mtaConsoleInputMode) {
+        return await textParser(`{f8}say ${macro.text}{enter}{f8}`);
+      }
 
-			if (__app.config.others.mtaConsoleInputMode) {
-				return await textParser(`{f8}${macro.text}{enter}{f8}`);
-			}
+      return await textParser(macro.text);
+    }
 
-			return await textParser(macro.text);
-		}
+    this.speechSynthesis.speak(__app.translations.speechFeedback.commandHandler.unknownMacro);
+    __app.console.log(__app.translations.textFeedback.commandHandler.unknownMacro);
+  }
 
-		this.speechSynthesis.speak(__app.translations.speechFeedback.commandHandler.unknownMacro);
-		__app.console.log(__app.translations.textFeedback.commandHandler.unknownMacro);
-	}
+  private async callCommand(command: string, availability: Command$Availability, ...args: any[]) {
+    if (!__app.config.commands.enabled) {
+      return;
+    }
 
-	private async callCommand(command: string, availability: Command$Availability, ...args: any[]) {
-		if (!__app.config.commands.enabled) {
-			return;
-		}
+    const commandArray = command.slice(1).replace(/\s+/g, '').split(__app.config.commands.splitter);
+    const cmd: CmdParsed = {
+      handler: commandArray[0],
+      args: commandArray.slice(1),
+    };
 
-		const commandArray = command.slice(1).replace(/\s+/g, '').split(__app.config.commands.splitter);
-		const cmd: CmdParsed = {
-			handler: commandArray[0],
-			args: commandArray.slice(1),
-		};
+    __app.console.debugLogJson({ cmd });
 
-		__app.console.debugLogJson({ cmd });
+    if (typeof command === 'string' && anyHas(cmd.handler, ['makró', 'makrók', 'szöveg', 'szövegek'])) {
+      return await this.callMacro(cmd.args[0], args.length > 1 ? args[1] : 'write');
+    }
 
-		if (typeof command === 'string' && anyHas(cmd.handler, ['makró', 'makrók', 'szöveg', 'szövegek'])) {
-			return await this.callMacro(cmd.args[0], args.length ? args[0] : 'write');
-		}
+    const filteredCommands = this.commands.filter((_command) =>
+      typeof _command.handler === 'object' ? anyHas(_command.handler, cmd.handler) : _command.handler === cmd.handler,
+    );
 
-		const filteredCommands = this.commands.filter((_command) => (typeof _command.handler === 'object' ? anyHas(_command.handler, cmd.handler) : _command.handler === cmd.handler));
+    if (filteredCommands.length > 0) {
+      const command = filteredCommands[0];
 
-		if (filteredCommands.length > 0) {
-			const command = filteredCommands[0];
+      if (command.availability !== 'both') {
+        if (command.availability !== availability) {
+          return;
+        }
 
-			if (command.availability !== 'both') {
-				if (command.availability !== availability) {
-					return;
-				}
+        return await command.func(this.speechSynthesis, ...args, ...cmd.args);
+      }
 
-				return await command.func(this.speechSynthesis, ...args, ...cmd.args);
-			}
+      return await command.func(this.speechSynthesis, ...args, ...cmd.args);
+    }
 
-			return await command.func(this.speechSynthesis, ...args, ...cmd.args);
-		}
+    this.speechSynthesis.speak(__app.translations.speechFeedback.commandHandler.unknownCommand);
+    __app.console.log(__app.translations.textFeedback.commandHandler.unknownCommand);
+  }
 
-		this.speechSynthesis.speak(__app.translations.speechFeedback.commandHandler.unknownCommand);
-		__app.console.log(__app.translations.textFeedback.commandHandler.unknownCommand);
-	}
+  async textCommandHandler(command: string, ...args: any[]) {
+    if (!command || command === undefined || command === null || command === '') {
+      return;
+    }
+    // if (!command.startsWith(__app.config.commands.prefix)) {
+    // 	return;
+    // }
 
-	async textCommandHandler(command: string, ...args: any[]) {
-		if (!command || command === undefined || command === null || command === '') {
-			return;
-		}
-		// if (!command.startsWith(__app.config.commands.prefix)) {
-		// 	return;
-		// }
+    await this.callCommand(command, 'text', ...args);
+  }
 
-		await this.callCommand(command, 'text', ...args);
-	}
+  async voiceCommandHandler(command: string, ...args: any[]) {
+    if (!command || command === undefined || command === null || command === '') {
+      return;
+    }
+    if (!command.startsWith(__app.config.commands.prefix)) {
+      return;
+    }
 
-	async voiceCommandHandler(command: string, ...args: any[]) {
-		if (!command || command === undefined || command === null || command === '') {
-			return;
-		}
-		if (!command.startsWith(__app.config.commands.prefix)) {
-			return;
-		}
+    return await this.callCommand(command, 'speech', ...args);
+  }
 
-		return await this.callCommand(command, 'speech', ...args);
-	}
-
-	async useCommand(command: string, ...args: any[]) {
-		return (await this.callCommand(command, 'both', ...args))!;
-	}
+  async useCommand(command: string, ...args: any[]) {
+    return (await this.callCommand(command, 'both', ...args))!;
+  }
 }
 
 export const cmd = new CommandHandler();
