d771693569a85427bc786cfbd0dc2b899737cad3
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-17 14:18:26 +0200

    refactor: clean up configuration files and improve formatting in jsconfig.json, svelte.config.js, tsconfig.json, and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from "path";
 import { UserConfig } from "vite";
 
 import { sveltekit } from "@sveltejs/kit/vite";
@@ -8,33 +7,17 @@ const isDev = process.env.NODE_ENV === "dev";
 const config: UserConfig = {
   plugins: [sveltekit()],
   build: {
-    minify: "esbuild",
-    terserOptions: {
-      compress: {
-        arrows: false,
-        passes: 1000000,
-        hoist_funs: true,
-        keep_fargs: false,
-        drop_console: isDev,
-        drop_debugger: isDev,
-        ecma: 2020,
-      },
-      safari10: true,
-      ecma: 2020,
-      format: {
-        ecma: 2020,
-      },
-    },
+    minify: !isDev ? "esbuild" : false,
     chunkSizeWarningLimit: 100000,
-  },
-  resolve: {
-    alias: {
-      $components: resolve("./src/components"),
-      $stores: resolve("./src/stores"),
-      $types: resolve("./src/types"),
-      $libs: resolve("./src/libs"),
-      $css: resolve("./src/css"),
-      $static: resolve("./static"),
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            const parts = id.split("node_modules/");
+            return parts[1].split("/")[0].toString();
+          }
+        },
+      },
     },
   },
 };
