6f8806bcd17892a3dda093094d41cb482a19b78a
energypatrikhu <energypatrikhu@proton.me>
2025-04-17 16:01:31 +0200

    fix: update import for svelte-preprocess to use named import

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 import adapter from "@sveltejs/adapter-static";
 import { resolve } from "path";
-import preprocess from "svelte-preprocess";
+import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: sveltePreprocess(),
 
   kit: {
     adapter: adapter({
