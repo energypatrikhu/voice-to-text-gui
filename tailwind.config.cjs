f3ed6a567d34cf3d48589b4554b717afbaac4475
energypatrikhu <energypatrikhu@proton.me>
2025-04-17 16:05:43 +0200

    style: format JSON and JavaScript files for improved readability

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			keyframes: {
-				resetScale: {
-					from: { transform: 'scale(0)' },
-					to: { transform: 'scale(1)' },
-				},
-				rotate: {
-					from: { transform: 'rotate(0deg)' },
-					to: { transform: 'rotate(360deg)' },
-				},
-			},
-			animation: {
-				resetScale: '1000ms resetScale calc(250ms + (var(--reval-delay) * 100ms)) forwards',
-				rotate: '1000ms rotate forwards infinite linear',
-			},
-		},
-	},
-	plugins: [require('tailwindcss-all')],
+  mode: "jit",
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      keyframes: {
+        resetScale: {
+          from: { transform: "scale(0)" },
+          to: { transform: "scale(1)" },
+        },
+        rotate: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        resetScale:
+          "1000ms resetScale calc(250ms + (var(--reval-delay) * 100ms)) forwards",
+        rotate: "1000ms rotate forwards infinite linear",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-all")],
 };
