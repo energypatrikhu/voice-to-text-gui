982f57e151f736fd6be4c583927abebf485fb9e6
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-17 14:18:16 +0200

    refactor: improve code formatting and structure in rm.mjs

--- rm.mjs
+++ rm.mjs
@@ -1,34 +1,40 @@
-import { readdirSync, rmSync, statSync } from 'fs';
-import { join, resolve } from 'path';
+import { existsSync, readdirSync, rmSync, statSync } from "fs";
+import { join, resolve } from "path";
 
 function removeFileOrDirectory(path) {
-	try {
-		rmSync(path, { recursive: true, force: true });
-		console.log('Removed:', path);
-	} catch {
-		console.log('Failed to remove:', path);
-	}
+  try {
+    rmSync(path, { recursive: true, force: true });
+    console.log("Removed:", path);
+  } catch {
+    console.log("Failed to remove:", path);
+  }
 }
 
 function removeDirectoryRecursively(absPath) {
-	for (const dirent of readdirSync(absPath, { withFileTypes: true })) {
-		const direntDir = join(absPath, dirent.name);
+  for (const dirent of readdirSync(absPath, { withFileTypes: true })) {
+    const direntDir = join(absPath, dirent.name);
 
-		if (dirent.isFile()) {
-			removeFileOrDirectory(direntDir);
-		} else if (dirent.isDirectory()) {
-			removeDirectoryRecursively(direntDir);
-		}
-	}
+    if (dirent.isFile()) {
+      removeFileOrDirectory(direntDir);
+    } else if (dirent.isDirectory()) {
+      removeDirectoryRecursively(direntDir);
+    }
+  }
 }
 
 for (const rawPath of process.argv.slice(2)) {
-	const path = resolve(rawPath);
-	const stat = statSync(path);
+  const path = resolve(rawPath);
 
-	if (stat.isFile()) {
-		removeFileOrDirectory(path);
-	} else if (stat.isDirectory()) {
-		removeDirectoryRecursively(path);
-	}
+  if (!existsSync(path)) {
+    console.log("Path does not exist:", path);
+    continue;
+  }
+
+  const stat = statSync(path);
+
+  if (stat.isFile()) {
+    removeFileOrDirectory(path);
+  } else if (stat.isDirectory()) {
+    removeDirectoryRecursively(path);
+  }
 }
